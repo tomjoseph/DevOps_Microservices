26fdd003f338066f7aaddbec3d6ebc5fd75c82d9
Tom Joseph <tj4tech@gmail.com>
2022-01-16 00:01:11 +0000

    Task 6 Kube

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=tjoseph2020/tj-app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run tj-app --image=tjoseph2020/tj-app --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl wait --for=condition=ready pods/tj-app
+kubectl get po -A
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward tj-app 8000:80
